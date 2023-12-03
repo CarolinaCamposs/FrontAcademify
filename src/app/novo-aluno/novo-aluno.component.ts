55efdf524a5f4536add7c61f91d700e9570a0b8a
Carolina Campos <carolinacampos.dev@gmail.com>
2023-12-03 16:45:30 -0300

    Atualizando

--- src/app/novo-aluno/novo-aluno.component.ts
+++ src/app/novo-aluno/novo-aluno.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+// novo-aluno.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlunoService } from '../aluno.service';
 
 @Component({
   selector: 'app-novo-aluno',
   templateUrl: './novo-aluno.component.html',
-  styleUrl: './novo-aluno.component.css'
+  styleUrls: ['./novo-aluno.component.css']
 })
-export class NovoAlunoComponent {
+export class NovoAlunoComponent implements OnInit {
+  alunoForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private alunoService: AlunoService) { }
+
+  ngOnInit(): void {
+    this.inicializarFormulario();
+  }
+
+  inicializarFormulario(): void {
+    this.alunoForm = this.formBuilder.group({
+      nome: ['', Validators.required],
+      matricula: ['', Validators.required],
+      nascimento: ['', Validators.required],
+      dataCadastro: [{ value: '', disabled: true }],
+    });
+  }
+
+  salvarAluno() {
+    if (this.alunoForm.valid) {
+      const alunoData = this.alunoForm.value;
+      this.alunoService.criarAluno(alunoData).subscribe(
+        (response: any) => {
+          console.log('Aluno salvo com sucesso:', response);
+        },
+        (error: any) => {
+          console.error('Erro ao salvar aluno:', error);
+        }
+      );
+    }
+  }
 }

ba61b0841939fb4c624a162ece10048abff885ed
Carolina Campos <carolinacampos.dev@gmail.com>
2023-12-08 00:19:55 -0300

    Adicionado icon para exibir detalhes dos alunos

--- src/app/aluno-lista/aluno-lista.component.ts
+++ src/app/aluno-lista/aluno-lista.component.ts
@@ -5,6 +5,9 @@ import { MatTableDataSource } from '@angular/material/table';
 import { Aluno } from '../aluno.model';
 import { AlunoService } from '../aluno.service';
 import { Router } from '@angular/router';
+import { AlunoDetalhesComponent } from '../aluno-detalhes/aluno-detalhes.component';
+import { MatDialog } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-aluno-lista',
@@ -18,7 +21,11 @@ export class AlunoListaComponent implements AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private alunoService: AlunoService, private router: Router) { } // Injete o Router no construtor
+  constructor(
+    private alunoService: AlunoService,
+    private router: Router,
+    private dialog: MatDialog
+  ) { } // Injete o Router no construtor
 
   ngAfterViewInit() {
     this.listarAlunos();
@@ -27,20 +34,26 @@ export class AlunoListaComponent implements AfterViewInit {
   listarAlunos(): void {
     this.alunoService.listarAlunos().subscribe((alunos: Aluno[]) => {
       console.log('Dados recebidos:', alunos);
-  
+
       alunos.forEach(aluno => {
         // Certifique-se de que aluno.nascimento é um objeto Date ou uma string no formato de data adequado.
         aluno.nascimento = new Date(aluno.nascimento);
       });
-  
+
       this.dataSource.data = alunos;
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
     });
   }
-  
-  
 
+  abrirDetalhesAluno(aluno: Aluno): void {
+    // Aqui você pode usar um serviço ou um modal para exibir os detalhes
+    // Por exemplo, usando um serviço de dialog do Angular Material
+    this.dialog.open(AlunoDetalhesComponent, {
+      width: '300px',
+      data: aluno,
+    });
+  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
@@ -55,7 +68,7 @@ export class AlunoListaComponent implements AfterViewInit {
   }
 
   editarAluno(alunoId: number): void {
-   
+
     this.router.navigate(['/editaraluno', alunoId]);
   }
 }
